38ce4a0fe217eaccd7987c10f99944494d55616e
JackyLin1 <jlin9702@gmail.com>
2022-01-20 11:33:38 +0900

    added some constants

--- client.js
+++ client.js
@@ -1,15 +1,18 @@
 const net = require("net");
+const { IP, PORT, name } = require("./constants");
+
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243' , // IP address here,
-    port: 50541// PORT number here,
+    host: IP , // IP address here,
+    port: PORT// PORT number here,
   });
   
   conn.on ('connect', () => {
     console.log("Successfully connected to game server")
-    conn.write('Name: MGS')
+    conn.write(`Name: ${name}`)
+    conn.write('Say: hello')
   })
 
   conn.on('data', (data) => {
---
+++ constants.js
@@ -0,0 +1,12 @@
+const IP = '165.227.47.243';
+const PORT = 50541;
+const name = process.argv.slice(2);
+
+
+
+module.exports = {
+  IP,
+  PORT,
+  name,
+
+};
